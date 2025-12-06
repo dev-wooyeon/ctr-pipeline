62331c1f2cbf1a5781c165ad1e65da9c71889287
dev-wooyeon <une@kakao.com>
2025-12-07 00:22:20 +0900

    Allow window function to accept nullable keys

--- flink-app/src/main/kotlin/com/example/ctr/domain/service/CTRResultWindowProcessFunction.kt
+++ flink-app/src/main/kotlin/com/example/ctr/domain/service/CTRResultWindowProcessFunction.kt
@@ -7,17 +7,17 @@ import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction.
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow
 import org.apache.flink.util.Collector
 
-class CTRResultWindowProcessFunction : ProcessWindowFunction<EventCount, CTRResult, String, TimeWindow>() {
+class CTRResultWindowProcessFunction : ProcessWindowFunction<EventCount, CTRResult, String?, TimeWindow>() {
 
     override fun process(
-        key: String,
+        key: String?,
         context: Context,
         elements: Iterable<EventCount>,
         out: Collector<CTRResult>
     ) {
         val counts = elements.iterator().next()
         val result = CTRResult.calculate(
-            key,
+            key ?: "<unknown>",
             counts.impressions,
             counts.clicks,
             context.window().start,
